fdfa0d71a49f675ef5118800f5e281e25d6d436f
KaMoni <monika.mang@dataone.de>
2013-10-25 09:06:18 +0200

    added initial version of Gruntfile.js and package.json

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		
+		pkg: grunt.file.readJSON('package.json'),
+		
+				clean: {
+			clean:['distribution']
+		}, 
+		
+			copy: {
+			main:{
+				src:'Projektname/**',
+				dest:'distribution',
+				flatten: false,
+				expand: true
+			}
+		}, 
+		
+		uglify: {
+			files: {
+				src: ['distribution/**/*.js'],
+				dest: '',
+				flatten: false,
+				expand: true
+			},
+			options: {
+				mangle: false,
+				// the banner is inserted at the top of the output
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			}
+		},
+	});
+	
+	//load plugins
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	
+	//Run these tasks with the grunt command -> all tasks are executed in the order listed below
+	//you can run them separately by executing grunt copy (grunt uglify, grunt clean) in the node.js command prompt
+	 grunt.registerTask('default', ['clean', 'copy', 'uglify']);
+};
